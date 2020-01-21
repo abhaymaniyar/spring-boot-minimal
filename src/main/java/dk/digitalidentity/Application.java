48e5e6cf488ece459dc785b63f88c3d27da013b5
Abhay Maniyar <abhaymaniyarhack@gmail.com>
2020-01-22 02:27:48 +0530

    Add something

--- src/main/java/dk/digitalidentity/Application.java
+++ src/main/java/dk/digitalidentity/Application.java
@@ -1,10 +1,13 @@
 package dk.digitalidentity;
 
+import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableZuulProxy
 @SpringBootApplication
 @ComponentScan("dk.digitalidentity.*")
 public class Application {
