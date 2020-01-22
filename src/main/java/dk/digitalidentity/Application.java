a2ea172e9c5eb20a2393b00f42a12bfdea0aacad
Abhay Maniyar <abhaymaniyarhack@gmail.com>
2020-01-22 20:58:33 +0530

    .

--- src/main/java/dk/digitalidentity/Application.java
+++ src/main/java/dk/digitalidentity/Application.java
@@ -1,17 +1,32 @@
 package dk.digitalidentity;
 
-import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.time.Duration;
 
-@EnableZuulProxy
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+
+
+//@EnableZuulProxy
 @SpringBootApplication
-@ComponentScan("dk.digitalidentity.*")
-public class Application {
+public class Application implements WebMvcConfigurer {
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		Refill refill = Refill.greedy(10, Duration.ofMinutes(1));
+		Bandwidth limit = Bandwidth.classic(10, refill).withInitialTokens(1);
+		Bucket bucket = Bucket4j.builder().addLimit(limit).build();
+		registry.addInterceptor(new RateLimitInterceptor(bucket, 1)).addPathPatterns("/hello");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
